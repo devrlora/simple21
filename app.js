b370bed964b9486f6658af8640d7381cb9f68559
Student_GA113 <Student_GA113@Student-GA113.local>
2020-03-12 21:56:11 -0400

    moved app.js out of pseudocode dir, add function outCome to app.js

--- app.js
+++ app.js
@@ -1,44 +1,57 @@
 /********************** Create welcome screen using 'alert' and explain rules of game *********************/
 // function greeting() {
-//         alert("Welcome to Simple 21! Which ever player gets a number closer to 21 wins the round. Best 3 out of 5 rounds wins. To start click OK then "
-//         + document.getElementById("startButton").innerText);
-//     };
+//     alert("Welcome to Simple 21! Which ever player gets a number closer to 21 wins the round. Best 3 out of 5 rounds wins. To start =>click OK<= then " +
+//         document.getElementById("startButton").innerText);
+// };
 
 /************************ Generate players' random numbers bet. 1-21 ************************/
+// let play1 = 0;
+// let play2 = 0;
 
-function allNum() {
+function newNum() {
     let play1 = Math.floor(Math.random() * (21) + 1);
     console.log(play1);
-
     let play2 = Math.floor(Math.random() * (21) + 1);
     console.log(play2);
-
     document.getElementById("numLeft").innerHTML = play1;
-    document.getElementById("numRight").innerHTML = play2;
-}
+    document.getElementById("numRight").innerHTML = play2
+    firstNum = play1;
+    secNum = play2;
+};
+
 /* Call function to display the random numbers into html */
-allNum();
+newNum();
+
+
 
 
 /************************ Write conditional based on higher number, displaying 
  * message if player wins or loses round ************************/
 
-// function whoWon() {
-//  if (play1 > play2) {
-//     console.log('You Win!');
-// } else if (play1 < play2) {
-//     console.log('Dealer Wins!');
-// } else {
-//     console.log('Draw!')
-// };
-//     document.getElementById("playerName").innerHTML = play1;
-//     document.getElementById("playerName").innerHTML = play2;
-// }
+function outCome() {
+    let iWon = "Human";
+    let iLost = "Dealer";
+    let noWin = "Draw";
 
-// whoWon();
+    if (firstNum > secNum) {
+        document.getElementById("playerName").innerHTML = iWon;
+    } else if (firstNum < secNum) {
+        document.getElementById("playerName").innerHTML = iLost;
+    } else if(firstNum===secNum){
+        document.getElementById("playerName").innerHTML = noWin;
+    }else {
+        console.log("Error");
+    }
+};
+
+outCome();
 
 
-
+function allNum() {
+    newNum();
+    outCome();
+}
+allNum();
 
 /******************* Output results to html form ********************* */
 // let form=document.getElementById("result");
