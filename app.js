515301402b85382be10d00fb48f4ad1d365a714b
Student_GA113 <Student_GA113@Student-GA113.local>
2020-03-13 14:34:06 -0400

    **Create a variable  i++ and execute in newNum function to display number of rounds played

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 /********************** Create welcome screen using 'alert' and explain rules of game *********************/
-// function greeting() {
-//     alert("Welcome to Simple 21! Which ever player gets a number closer to 21 wins the round. Best 3 out of 5 rounds wins. To start =>click OK<= then " +
-//         document.getElementById("startButton").innerText);
-// };
+function greeting() {
+    alert("Welcome to Simple 21! Which ever player gets a number closer to 21 wins the round. Best 3 out of 5 rounds wins. To start =>click OK<= then " +
+        document.getElementById("startButton").innerText);
+};
 
 /************************ Generate players' random numbers bet. 1-21 ************************/
 // let play1 = 0;
 // let play2 = 0;
-
+let i=0;
 function newNum() {
     let play1 = Math.floor(Math.random() * (21) + 1);
     console.log(play1);
@@ -17,16 +17,20 @@ function newNum() {
     document.getElementById("numRight").innerHTML = play2
     firstNum = play1;
     secNum = play2;
+    i++
+    outCome()
+    console.log(i+ "aaabbbccc")
+    document.getElementById("numRound").innerHTML = i;
 };
 
 /* Call function to display the random numbers into html */
-newNum();
+//newNum();
 
 
 
 
-/************************ Write conditional based on higher number, displaying 
- * message if player wins or loses round ************************/
+/************************ Write conditional based on comparing higher number, displaying 
+ * message if player wins, loses or draws ************************/
 
 function outCome() {
     let iWon = "You Win!";
@@ -37,44 +41,102 @@ function outCome() {
         document.getElementById("playerName").innerHTML = iWon;
     } else if (firstNum < secNum) {
         document.getElementById("playerName").innerHTML = iLost;
-    } else if(firstNum===secNum){
+    } else if (firstNum === secNum) {
         document.getElementById("playerName").innerHTML = noWin;
-    }else {
+    } else {
         console.log("Error");
     }
 };
-
-outCome();
-
-
-function allNum() {
-    newNum();
-    outCome();
-}
-allNum();
-
-/******************* Output results to html form ********************* */
-// let form=document.getElementById("result");
-// form.addEventListener('click',function(){
-// });
-
-
-/******************* Create counter for number "Round"/ 
- * Onclick=> update "Round" number ************************/
-
-
-// id="numRound"
+// outCome();
 
 
 
+/******************* Output all function results to html ********************* */
 
-/******************* create end of game after 5 rounds********************* */
-// let score = 0
+// function allNum() {
+//     newNum();
+//     outCome();
+//     rNum();
+// };
+// allNum();
 
-// for(let i = 0; i < 5; i++) {
-//   if( )
 
+/******************* Create counter for "Round" number/ 
+ * Onclick=>of start button, update "Round" number ************************/
+// let count = 0;
+// let button = document.getElementById("clickPlay");
+// let display = document.getElementById("displayCount");
+
+// clickPlay.onclick = function () {
+//     count++;
+//     display.innerHTML = count;
 // }
 
-// return score
+// function rNum() {
+    // let clickPlay = document.getElementById("numRound");
+    // let numRound = document.getElementById("numRound");
+    // numRound.innerHTML = 0;
+    // clickPlay.onclick = function()
+    // {
+    //     numRound.innerHTML++;
+    // };
+
+
+
+// rNum()
+// function rNum() {
+// let button = document.getElementById("clickPlay"),
+//   count = 0;
+// button.onclick = function() {
+//   count += 1;
+//   button.innerHTML = "Round Number: " + count;
 // }
+// };
+// rNum()
+
+
+
+// let Score=0;
+// if (iWon === true) 
+//         {
+//             Score=Score+1;
+//     }
+//     else 
+//     {
+//             Score=Score-1;
+//     }
+
+//     var lblScore = Document.getElementById('lblScore');
+//     lblScore.innerHTML=Score
+//     clickPlay.onclick = function()
+//     {
+//         clickPlay.innerHTML++;
+//     };
+
+
+
+// let displayCount=document.getElementById("displayCount");
+
+// displayCount.EventListener("click",function(){
+//     clickPlay.onclick++;
+//         });
+//let i =0
+
+
+/******************* create end of game after 5 rounds ********************* */
+
+// function endGame() {
+//     let outCome = 0;
+//     let rNum = document.getElementById("resultLeft"),
+//         count = 0;
+//     while (rNum < 5) {
+//         text += "The number is " + i;
+//         i++;
+//         document.getElementById("resultLeft").innerHTML = outCome;
+//     }
+// }
+// endGame();
+
+// if(outCome=)
+
+/******************* Create reset button functionality ********************* */
