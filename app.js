6a37906ea4f02e81b3731dae7bb99f079b546e16
Student_GA113 <Student_GA113@Student-GA113.local>
2020-03-15 01:48:04 -0400

    removed commented broken code and add proper spacing and indentation

--- app.js
+++ app.js
@@ -1,55 +1,62 @@
-/********************** Create welcome screen using 'alert' and explain rules of game *********************/
+/****************** Welcome screen using 'alert' to explain rules of game ****************/
 function greeting() {
     alert("Welcome to Simple 21! Which ever player gets a number closer to 21 wins the round. Best 3 out of 5 rounds wins. To start =>click OK<= then " +
         document.getElementById("startButton").innerText);
 };
-
-/************************ Generate players' random numbers bet. 1-21 ************************/
-// let play1 = 0;
-// let play2 = 0;
-let i=0;
+/* Declare random number,end game message, number of rounds won variables */
+let i = 0;
 let gameOver = "Game Over";
 let playerWon = 0;
 let compWon = 0;
-    
+
+/***************************** Click start button executes block code:  ***************************/
+
 function newNum() {
+
+    /* Generate players' random numbers bet. 1-21 */
+
     let play1 = Math.floor(Math.random() * (21) + 1);
     console.log(play1);
     let play2 = Math.floor(Math.random() * (21) + 1);
     console.log(play2);
+
+    /* Assign the generated number variables to display into HTML when start button is pressed */
+
     document.getElementById("numLeft").innerHTML = play1;
-    document.getElementById("numRight").innerHTML = play2
+    document.getElementById("numRight").innerHTML = play2;
+
     firstNum = play1;
     secNum = play2;
+
+    /* Assign variable (i) as a counter for round number */
+
     i++
     outCome()
     console.log(i)
     document.getElementById("numRound").innerHTML = i;
-    if (i==5){
+
+    /* Limit game to 5 rounds, display message when game has ended,
+    assign number of rounds won for each player, hide play button */
+
+    if (i == 5) {
         document.getElementById("endTheGame").innerHTML = "Game Over";
         clickPlay.style.display = "none";
     };
-    if (play1>play2){
+    if (play1 > play2) {
         playerWon++
         document.getElementById("displayCount1").innerHTML = playerWon;
-    } else if(play1<play2){
+    } else if (play1 < play2) {
         compWon++
         document.getElementById("displayCount2").innerHTML = compWon;
     }
-        if (i==5){
+    if (i == 5) {
         document.getElementById("playerName").innerHTML = "";
         clickPlay.style.display = "none";
     }
 };
-/* Calls function to display the random numbers into html when button is pressed */
 
-//newNum();
-
-
-
-
-/************************ Write conditional based on comparing higher number, displaying 
- * message if player wins, loses or draws ************************/
+/* Conditional based on comparing higher number, displaying message if player wins, 
+/* loses or draws upon click of start button to html */
 
 function outCome() {
     let iWon = "You Win!";
@@ -66,101 +73,3 @@ function outCome() {
         console.log("Error");
     }
 };
-// outCome();
-
-
-
-/******************* Output all function results to html ********************* */
-
-// function allNum() {
-//     newNum();
-//     outCome();
-//     rNum();
-// };
-// allNum();
-
-
-/******************* Create counter for "Round" number/ 
- * Onclick=>of start button, update "Round" number ************************/
-// let count = 0;
-// let button = document.getElementById("clickPlay");
-// let display = document.getElementById("displayCount");
-
-// clickPlay.onclick = function () {
-//     count++;
-//     display.innerHTML = count;
-// }
-
-// function rNum() {
-    // let clickPlay = document.getElementById("numRound");
-    // let numRound = document.getElementById("numRound");
-    // numRound.innerHTML = 0;
-    // clickPlay.onclick = function()
-    // {
-    //     numRound.innerHTML++;
-    // };
-
-
-
-// rNum()
-// function rNum() {
-// let button = document.getElementById("clickPlay"),
-//   count = 0;
-// button.onclick = function() {
-//   count += 1;
-//   button.innerHTML = "Round Number: " + count;
-// }
-// };
-// rNum()
-
-
-
-// let Score=0;
-// if (iWon === true) 
-//         {
-//             Score=Score+1;
-//     }
-//     else 
-//     {
-//             Score=Score-1;
-//     }
-
-//     var lblScore = Document.getElementById('lblScore');
-//     lblScore.innerHTML=Score
-//     clickPlay.onclick = function()
-//     {
-//         clickPlay.innerHTML++;
-//     };
-
-
-
-// let displayCount=document.getElementById("displayCount");
-
-// displayCount.EventListener("click",function(){
-//     clickPlay.onclick++;
-//         });
-//let i =0
-
-
-/******************* create end of game after 5 rounds ********************* */
-
-
-   
-
-
-
-//     let rNum = document.getElementById("resultLeft"),
-//         count = 0;
-//     while (rNum < 5) {
-//         text += "The number is " + i;
-//         i++;
-//         document.getElementById("resultLeft").innerHTML = outCome;
-//     }
-// }
-// endGame();
-
-// if(outCome=)
-
-/******************* Create reset button functionality ********************* */
-/* added <button id="clickPlay" to reload page when clicked */
-
